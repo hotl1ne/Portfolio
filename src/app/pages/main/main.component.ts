d1327364661e704b39321748f59bddbdc10664f1
hotl1ne <m.kurychja@gmail.com>
2025-02-01 22:10:02 +0200

    updated project section

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeadearComponent } from "../../components/headear/headear.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { ProjectCardComponent } from "../../components/project-card/project-card.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeadearComponent, FooterComponent],
+  imports: [HeadearComponent, FooterComponent, ProjectCardComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
